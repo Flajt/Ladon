7d44bc78207b16c072cd35a6397f4a30749f9249
Flajt <matrimkurz@gmail.com>
2023-01-17 16:33:43 +0100

    Chore: Remove dead code

--- ladon/lib/pages/ViewOtpsPage.dart
+++ ladon/lib/pages/ViewOtpsPage.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:ladon/features/otp/blueprints/OtpBlueprint.dart';
 import 'package:ladon/features/otp/uiblocks/OtpTile.dart';
