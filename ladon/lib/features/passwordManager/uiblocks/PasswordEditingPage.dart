4336b7b21cd5069485c4f29de4bb8b198703db20
Flajt <matrimkurz@gmail.com>
2023-01-17 16:31:31 +0100

    Fix: Remove avatar, change color, change headline, allow resizing

--- ladon/lib/features/passwordManager/uiblocks/PasswordEditingPage.dart
+++ ladon/lib/features/passwordManager/uiblocks/PasswordEditingPage.dart
@@ -51,56 +51,37 @@ class _PasswordEditingPageState extends State<PasswordEditingPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-          child: SizedBox(
-        child: Stack(children: [
-          Positioned(
-              top: size.height * .05,
-              left: size.width * .05,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: size.width - 50,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Title(
-                          color: Theme.of(context).primaryColor,
-                          child: Text(
-                            blueprint.label,
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline2!
-                                .copyWith(
-                                    color: Theme.of(context).primaryColor),
-                          )),
-                      CircleAvatar(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        child: blueprint.logoUrl?.isNotEmpty ?? false
-                            ? Image.network(blueprint.logoUrl!)
-                            : const Icon(Icons.question_mark),
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-          Align(
-              alignment: Alignment.center,
-              child: Container(
-                  padding: const EdgeInsets.all(12.0),
-                  width: size.width * .8,
-                  height: size.height * .5,
-                  child: ServiceColumn(
-                      labelController: labelController,
-                      usernameController: usernameController,
-                      passwordController: passwordController,
-                      appNameController: appNameController,
-                      urlController: urlController)))
-        ]),
-        width: size.width,
-        height: size.height,
-      )),
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: SafeArea(
+            child: SizedBox(
+          width: size.width,
+          height: size.height,
+          child: Stack(children: [
+            Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: EdgeInsets.only(top: size.height * .1),
+                  child: Title(
+                      color: Colors.black,
+                      child: Text(blueprint.label,
+                          style: Theme.of(context).textTheme.headline3!)),
+                )),
+            Align(
+                alignment: Alignment.center,
+                child: Container(
+                    padding: const EdgeInsets.all(12.0),
+                    width: size.width * .8,
+                    height: size.height * .5,
+                    child: ServiceColumn(
+                        labelController: labelController,
+                        usernameController: usernameController,
+                        passwordController: passwordController,
+                        appNameController: appNameController,
+                        urlController: urlController)))
+          ]),
+        )),
+      ),
       floatingActionButton: DeleteServiceButton(
           serviceBlueprint: blueprint, passwordManager: PasswordManager()),
     );
