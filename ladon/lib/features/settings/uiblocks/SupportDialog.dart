78eb84ce33b11d9b27eb08d4c4d56032b39d74a7
Flajt <matrimkurz@gmail.com>
2023-03-17 15:45:31 +0100

    Feat: Add support dialog

---
+++ ladon/lib/features/settings/uiblocks/SupportDialog.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_donation_buttons/donationButtons/buyMeACoffeeButton.dart';
+import 'package:flutter_donation_buttons/donationButtons/ko-fiButton.dart';
+
+class SupportDialog extends StatelessWidget {
+  const SupportDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: SizedBox(
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Text("Support"),
+            BuyMeACoffeeButton(buyMeACoffeeName: "flajt"),
+            KofiButton(kofiName: "flajt", kofiColor: KofiColor.Orange)
+          ],
+        ),
+      ),
+    );
+  }
+}
